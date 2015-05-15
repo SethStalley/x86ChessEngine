54d003c97f5b454319cc35ddac927b3e7a912fdf
seth <sethstalley@gmail.com>
2015-05-15 12:46:10 -0600

    added board.asm working on bitboard initial board piece placement

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ TARGET =  main	#main target to compile
 NAME = Chess
 
 all: $(TARGET:=.o)
-	gcc -m32 $(TARGET:=.o) -o $(NAME) 
+	gcc -m32 $(TARGET:=.o) board.o -o $(NAME) 
 
-$(TARGET:=.o): clean
+$(TARGET:=.o): board
 	nasm -felf32 $(TARGET:=.asm)
+
+board: clean
+	nasm -felf32 board.asm
 
 clean:
 	-rm -f $(TARGET:=.o)
--- main.asm
+++ main.asm
@@ -1,15 +1,18 @@
- 	extern printf
+ 	extern printf, initBoard
 section .data
-	msg    db "Hello",10,0
+	msg    db "%d",10,0
 section .text
     	global main
 
 main:	
 	pusha
-	push dword msg	;pointer in stack to msg
+	call initBoard
+	push eax
+	push msg
     	call printf	;c function to call
-	add esp, 4	;pop stack
+	add esp,8	;pop stack
 	popa
+	
 
     	xor eax, eax	;exit code
 	ret
