198a42c31f7eccd22c86a159130a9db919e3e718
seth <sethstalley@gmail.com>
2015-05-16 18:00:58 -0600

    working on basic pawn mov

--- board.asm
+++ board.asm
@@ -3,56 +3,62 @@ extern	printf
 
 section .data
 	printD			db	"%016llx",10,0
+	printHere		db	"Here",10,0
 
-	blackBoard dd 0x0		;used to store black pieces when calc
-	whiteBoard dd 0x0		;used to store white pieces when calc
+
+
+
+	blackBoard dq 0x0		;used to store black pieces when calc
+	whiteBoard dq 0x0		;used to store white pieces when calc
 
 					;bitboards for white
-	whitePawns 	dq 	0xff00
+	whitePawns 	  dq 	0xff00
 	whiteBishops 	dq	0x24
 	whiteKnights 	dq 	0x42
 	whiteCastles 	dq 	0x81
 	whiteQueens 	dq 	0x8
-	whiteKing 	dq 	0x10
+	whiteKing 	  dq 	0x10
 
 					;bitboards for black
-	blackPawns 	dq 	0xff000000000000
+	blackPawns  	dq 	0xff000000000000
 	blackBishops 	dq 	0x2400000000000000
 	blackKnights 	dq 	0x4200000000000000
 	blackCastles	dq	0x8100000000000000
 	blackQueens 	dq 	0x800000000000000
-	blackKing	dq	0x1000000000000000
+	blackKing	    dq	0x1000000000000000
 
 section .bss
 	resb 1024
-section .code
+section .text
 aiMove:
-	mov rax, [blackCastles]
+	call fillWhiteBoard
+
+	mov rax, [whitePawns]
 	call calcMove
 	ret
+
 
 ;-------------------------------
 ;Push lower and upper bitmap
 ;Push piece mov function
 ;-------------------------------
-calcMove:		
+calcMove:
 	mov rcx, 0x1
 loopPieces:
 	push rax
 	push rcx
 
-	xor cx, cx
-	mov rax, [whitePawns]
-	call pawnMove
+	and rcx, rax
+	call pawnMove						;call the procedure for that piece
 
 	mov rsi, [whitePawns]
+	;mov rsi, rcx
 	mov edi, printD
 	mov eax, 0
-	call printf
-
-	
+	;call printf
 	pop rcx
 	pop rax
+
 
 	shl rcx, 0x1
 	cmp rcx, 0x8000000000000000
@@ -65,56 +71,71 @@ endMovCalc:
 ;-------------------------------
 fillBlackBoard:
 	push rax
+	push rcx
+	push rbx
+	mov rcx, 6
+	mov rbx, blackPawns					;start of black bitboards address
 	xor rax, rax
-	or rax, [blackPawns]
-	or rax, [blackBishops]
-	or rax, [blackKnights]
-	or rax, [blackCastles]
-	or rax, [blackQueens]
-	or rax, [blackKing]
+loopfillBlackBoard:						;loop through the bitboards
+	or rax, [rbx + rcx * 8]
+	dec rcx
+	cmp rcx, 0
+	jne loopfillBlackBoard
 	mov [blackBoard], rax
+	pop rbx
+	pop rcx
 	pop rax
 	ret
+
 ;-------------------------------
 ;Fill board with all white positions
 ;-------------------------------
-fillWhiteBoard:
+fillWhiteBoard:								;same as fillBlackBoard but white
 	push rax
+	push rcx
+	push rbx
+	mov rcx, 6
+	mov rbx, whitePawns
 	xor rax, rax
-	or rax, [whitePawns]
-	or rax, [whiteBishops]
-	or rax, [whiteKnights]
-	or rax, [whiteCastles]
-	or rax, [whiteQueens]
-	or rax, [whiteKing]
+loopfillWhiteBoard:
+	or rax, [rbx + rcx * 8]
+	dec rcx
+	cmp rcx, 0
+	jne loopfillWhiteBoard
 	mov [whiteBoard], rax
+	pop rbx
+	pop rcx
 	pop rax
 	ret
 
 ;-------------------------------
-;Pawn movement AI, send it pawn 
+;Pawn movement AI, send it pawn
 ;bitboard in rax and cx = 0 white
 ;-------------------------------
 pawnMove:
-	cmp cx, 0		;if dd then white
-	jne blackPawn
+	;cmp cx, 0		;if dd then white
+	;jne blackPawn
 whitePawn:
-	call fillWhiteBoard
-	
-	push rax
-	add rax, 8		;check one move forward
+	shl rcx, 0x8		;check one move forward
 
-	mov rcx, [whiteBoard]
-	not rcx
-	and rax, rcx
-	cmp rax, 0		;if not posible move
+	mov rsi, rcx
+	mov edi, printD
+	mov eax, 0
+	call printf
+
+	mov rax, [whiteBoard]
+	not rax
+	and rcx, rax
+	cmp rcx, 0		;if not posible move
 	je donePawnMove
-	
-	xor [whitePawns], rax
+
+	;mov edi, printHere
+	;mov eax, 0
+	;call printf
+
+	xor [whitePawns], rcx		;make the pawnMove
 	jmp donePawnMove
 blackPawn:
 	call fillBlackBoard
 donePawnMove:
-	pop rax
->>>>>>> 5d3d20981592348d6b2b2a9484aaea648e78b83f
-	ret
+	ret						;end pawn move
