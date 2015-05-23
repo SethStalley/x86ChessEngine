ec69c95d4cc594fc0dd05e1de8f1bf8a89c76330
seth <sethstalley@gmail.com>
2015-05-22 22:57:54 -0600

    more work on negamax, added board saving and restore

--- board.asm
+++ board.asm
@@ -3,23 +3,23 @@ extern	printf
 
 section .data
 	printH		db	"%016llx",10,0	;print in hex 64bit
-	printD		db	"%d",10,0		;print normal decimal
-	printHere	db	"Here",10,0		;test msg
+	printD		db	"%d",10,0	;print normal decimal
+	printHere	db	"Here",10,0	;test msg
 
 
-	aiDepth		db	2		;depth for negaMax tree
-	aiPlayer		db 	1		;if ai is black/white default black
+	aiDepth		db	2	;depth for negaMax tree
+	aiPlayer	db 	1	;if ai is black/white default black
 
-	blackBoard dq 0x0		;used to store black pieces when calc
-	whiteBoard dq 0x0		;used to store white pieces when calc
+	blackBoard 	dq 	0x0	;used to store black pieces when calc
+	whiteBoard 	dq 	0x0	;used to store white pieces when calc
 
-							;bitboards for white
-	whitePawns 		dq 	0xff00
+					;bitboards for white
+	whitePawns 	dq 	0xff00
 	whiteBishops 	dq	0x24
 	whiteKnights 	dq 	0x42
 	whiteCastles 	dq 	0x81
 	whiteQueens 	dq 	0x8
-	whiteKing 		dq 	0x10
+	whiteKing 	dq 	0x10
 
 							;bitboards for black
 	blackPawns  	dq 	0xff000000000000
@@ -27,7 +27,7 @@ section .data
 	blackKnights 	dq 	0x4200000000000000
 	blackCastles	dq	0x8100000000000000
 	blackQueens 	dq 	0x800000000000000
-	blackKing	    dq	0x1000000000000000
+	blackKing    	dq	0x1000000000000000
 
 section .bss
 	resb 1024
@@ -53,42 +53,65 @@ aiMove:
 ;Expects depth in cl
 ;--------------------------------------
 ai:
-	call pushGame					;push all bitboards/game state to stack
-
 	cmp cl, 0
-	je doneAi						;reached bottom of our search tree
-	dec cl							;dec tree search depth
+	je doneAi	;reached bottom of our search tree
+	dec cl		;dec tree search depth
 
-	mov rax, -300					;worse case score
-	push rax						;push score
+	mov rax, -300	;worse case score
 
-	mov dl, 12						;loop 12 times one for each piece type (each player)
-allMoves:							;loop over all players posible moves
+			;check moves for all unique piece types on given side
+	mov dl, 0	
+allMoves:		;loop over all players posible moves
+	inc dl		;move to next piece type to check
 
-	call ai							;recurse
-	pop rbx							;pop last max
-	imul rbx, -1					;negate returned value from eval
-	cmp rbx, rax					;is new score (rbx) higher?
+			;do move				
+
+nextOfSamePieceType:	;continue for same piece type
+	;swap player
+	push rax
+	mov al, ch
+	imul rax, -1	
+	mov ch, al
+	pop rax
+
+	;push all bitboards
+	jmp pushGame	
+afterGamePush:
+
+	push rax	;push score
+	call ai		;recurse
+	pop rbx		;pop last max
+
+	;restore all bitboards
+	jmp popGame	
+afterGamePop:
+			;undo move
+
+
+	imul rbx, -1	;negate returned value from eval 
+	cmp rbx, rax	;is new score (rbx) higher?
 	jg swapMaxScore
 	jmp nextMove
 swapMaxScore:
-	mov rax, rbx					;swap max with score
+	mov rax, rbx	;swap max with score
+			;store the best mov
 nextMove:
+
 	cmp dl, 12
 	jne allMoves
-
 doneAi:
-	mov rbx, whitePawns				;this needs to be dynamic
-	call eval						;get an evaluation
-	ret								;done
+	mov rbx, whitePawns	;this needs to be dynamic
+	call eval		;get an evaluation
+	ret			;done
 
 
 ;-------------------------------
 ;Push all bitboard into stack
 ;Negamax uses this to save game state
 ;as it checks all moves
+;For every push we need to do a pop
 ;-------------------------------
-pushGame:					;push all bitboards down into stack
+pushGame:			;push all bitboards down into stack
 	push qWord [whitePawns]
 	push qWord [whiteBishops]
 	push qWord [whiteKnights]
@@ -104,44 +127,32 @@ pushGame:					;push all bitboards down into stack
 	push qWord [blackQueens]
 	push qWord [blackKing]
 
-	add rsp, 8 * 12			;set pointer back up 8 * 12 bytes in stack
-	ret
+	jmp afterGamePush
 
 ;-------------------------------
-;Push lower and upper bitmap
-;Push piece mov function
-; won't need this anymore moving to ai procedure
+;Pop back all bitboards from stack
 ;-------------------------------
-calcMove:
-	mov rcx, 0x1
-loopPieces:
-	push rax
-	push rcx
+popGame:
+	pop qWord [blackKing]
+	pop qWord [blackQueens]
+	pop qWord [blackCastles]
+	pop qWord [blackKnights]
+	pop qWord [blackBishops]
+	pop qWord [blackPawns]
 
-	;and rcx, rax
-	;call pawnMove				;call the procedure for that piece
+	pop qWord [whiteKing]
+	pop qWord [whiteQueens]
+	pop qWord [whiteCastles]
+	pop qWord [whiteKnights]
+	pop qWord [whiteBishops]
+	pop qWord [whitePawns]
 
-	;mov rbx, whitePawns		;test of eval procedure
-	;call eval
-
-	;mov rsi, rax				;temp print stuff
-	;mov edi, printD
-	;mov eax, 0
-	;call printf
-
-	pop rcx
-	pop rax
-
-
-	shl rcx, 0x1
-	cmp rcx, 0x8000000000000000
-	jne loopPieces
-endMovCalc:
-	ret
+	jmp afterGamePop
 
 ;-------------------------------
 ;Evaluates a players side against the oposite
 ;Expects address of pawn's bitboard of player to eval in rbx!
+;Sned player to evar in rcx, 1 = white | -1 = black
 ;------------------
 ;values:
 ;pawns = 1
@@ -152,23 +163,29 @@ endMovCalc:
 ;-------------------------------
 eval:
 	push rcx
-	popcnt rax, [rbx]			;count how many bits are on
-	popcnt rcx, [rbx+8]			;bishop bitBoard
-	imul rcx, 3					;bishop weight
-	add rax, rcx				;add it to values
+	popcnt rax, [rbx]		;count how many bits are on
+	popcnt rcx, [rbx+8]		;bishop bitBoard
+	imul rcx, 3			;bishop weight
+	add rax, rcx			;add it to values
 	popcnt rcx, [rbx+16]		;kights to bitboard
-	imul rcx, 3					;knight weight
+	imul rcx, 3			;knight weight
 	add rax, rcx
 	popcnt rcx, [rbx+24]		;rooks
-	imul rcx, 5					;rook value
+	imul rcx, 5			;rook value
 	add rax, rcx
 	popcnt rcx, [rbx+32]		;queens
-	imul rcx, 9					;queen value
+	imul rcx, 9			;queen value
 	add rax, rcx
 	popcnt rcx, [rbx+40]		;still have a king?
-	imul rcx, 200				;king's value
+	imul rcx, 200			;king's value
 	add rax, rcx
 	pop rcx
+
+	push rdx
+	xor edx, edx
+	mov dl, ch			;what player side
+	imul rax, rdx			;mul by player color
+	pop rdx
 	ret							;end of procedure
 
 ;-------------------------------
@@ -213,29 +230,29 @@ loopfillWhiteBoard:
 	pop rax
 	ret
 
-;-------------------------------
+;--------------------------------
 ;Pawn movement AI, send it pawn
-;bitboard in rax and cx = 0 white
-;-------------------------------
+; cx = 1 white
+;--------------------------------
 pawnMove:
-	;cmp cx, 0					;if dd then white
+	;cmp cx, 1		;if dd then white
 	;jne blackPawn
 whitePawn:
-	push rcx					;save original pawns place
-	shl rcx, 0x8				;check one move forward
+	push rcx		;save original pawns place
+	shl rcx, 0x8		;check one move forward
 
 	mov rax, [whiteBoard]
 	not rax
 	and rcx, rax
-	pop rax						;get roginal pawn pos back in rax
+	pop rax			;get original pawn pos back in rax
 
-	cmp rcx, 0					;if not posible move we are done
+	cmp rcx, 0		;if not posible move we are done
 	je donePawnMove
 
-	xor [whitePawns], rax		;eliminate the pawn to move
-	xor [whitePawns], rcx		;make the pawnMove
+	xor [whitePawns], rax	;eliminate the pawn to move
+	xor [whitePawns], rcx	;make the pawnMove
 	jmp donePawnMove
 blackPawn:
 	call fillBlackBoard
 donePawnMove:
-	ret							;end pawn move
+	ret			;end pawn move
